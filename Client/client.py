07ef5ae29f11ed79dfe7d0e36c4d7be2b89695c1
LastLaughingLobster <henriquecruz_neto@icloud.com>
2021-09-26 15:16:23 -0300

    Adding Diffie-Hellman and Xor cypher

--- Client/client.py
+++ Client/client.py
@@ -1,35 +1,76 @@
+from bitarray import bitarray
 import socket
 import sys
 
 SERVER_ACCESS = ('localhost', 5100)
 
+PUBLIC_PRIME = 997
+PUBLIC_ROOT = 821
+
+PRIVATE_KEY = 404
+PRIVATE_COMMOM_KEY = 0
+
 def client(SERVER_ACCESS):
 
-    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start():
-        print('connecting to %s port %s' % SERVER_ACCESS, file=sys.stderr)
-        _socket.connect(SERVER_ACCESS)
 
-        try:
-            message = 'This is the message.  It will be repeated.'
-            print('sending "%s"' % message, file=sys.stderr)
-            send_string(message)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
 
-            amount_received = 0
-            amount_expected = len(message)
+            print('connecting to %s port %s' % SERVER_ACCESS, file=sys.stderr)
+            _socket.connect(SERVER_ACCESS)
+
+            public_key = PUBLIC_PRIME ** PRIVATE_KEY % PUBLIC_ROOT
+
+            public_key = str(public_key)
+
+            print('sending "%s"' % public_key, file=sys.stderr)
+            _socket.sendall(bytes(public_key, 'utf-8'))
+
+            while True:
+                data =  _socket.recv(64)
+                if data:
+                    print('received "%s"' % data, file=sys.stderr)
+                    data = int(data.decode('utf-8'))
+                    PRIVATE_COMMOM_KEY = data ** PRIVATE_KEY % PUBLIC_ROOT
+                    print('PRIVATE COMMON KEY == {}'.format(PRIVATE_COMMOM_KEY))
+                    _socket.sendall(bytes('ACK', 'utf-8'))
+                    break
+                else:
+                    break
+
+            message = "This message has beeing encrypted"
+            print('Sending message: "%s"' % message, file=sys.stderr)
+
+            message =  xor_cypher(message, str(PRIVATE_COMMOM_KEY))
+            print('Encrypted format: "%s"'% message, file=sys.stderr)
+            _socket.sendall(bytes(message, 'utf-8'))
             
-            while amount_received < amount_expected:
-                data =  _socket.recv(32)
-                amount_received += len(data)
-                print('received "%s"' % data, file=sys.stderr)
+    
+    def xor_cypher(string, key):
 
-        finally:
-            print('closing socket', file=sys.stderr)
-            _socket.close()
+        def xor(bit1, bit2):
+            return (bit1 + bit2) - 2*(bit1*bit2)
 
-    def send_string(string):
-        _socket.sendall(bytes(string, 'utf-8'))
+        if len(string) > len(key):
+            key += (len(string) - len(key)) * '@'
+        else:
+            string += ( len(key) - len(string)) * '@'
+        
+        out = bitarray()
+
+        bit_string = bitarray()
+        bit_key = bitarray()
+
+        bit_string.frombytes(string.encode('utf-8'))
+        bit_key.frombytes(key.encode('utf-8'))
+
+        for S_bit, K_bit in zip(bit_string, bit_key):
+            out.append(xor(S_bit, K_bit))
+
+        out = bitarray(out.tolist()).tobytes().decode('utf-8')
+
+        return out
 
     return start
 
--- Server/server.py
+++ Server/server.py
@@ -1,43 +1,123 @@
+from bitarray import bitarray
 import socket
 import sys
 
 SERVER_PORT = 5100
+SERVER_ADRESS = 'localhost'
+
+PUBLIC_PRIME = 997
+PUBLIC_ROOT = 821
+
+PRIVATE_KEY = 505
+PRIVATE_COMMOM_KEY = 0
 
 def server(SERVER_PORT):
 
     def start():
-        _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        server_address = ('localhost', SERVER_PORT)
-        print('starting up on %s port %s' % server_address, file=sys.stderr)
-        _socket.bind(server_address)
+        #GEnerate private key
 
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
 
-        _socket.listen(1)
+            server_address = (SERVER_ADRESS, SERVER_PORT)
+            print('starting up on %s port %s' % server_address, file=sys.stderr)
+            _socket.bind(server_address)
 
-        while True:
+            _socket.listen(1)
+           
             print('waiting for a connection', file=sys.stderr)
             connection, client_address = _socket.accept()
 
-            try:
+            with connection:
+
                 print('connection from', client_address, file=sys.stderr)
 
+                public_key = PUBLIC_PRIME ** PRIVATE_KEY % PUBLIC_ROOT
+
+                public_key = str(public_key)
+
+                #KEY EXCHANGE
                 while True:
-                    data = connection.recv(32)
-                    print('received "%s"' % data, file=sys.stderr)
+
+                    data = connection.recv(64)
+                    
                     if data:
-                        print('sending data back to the client', file=sys.stderr)
-                        connection.sendall(data)
+                        if(data.decode('utf-8') == "ACK"):
+                            break
+
+                        print('Received public key "%s" from Bob' % data, file=sys.stderr)
+                    
+                        print('Sending my public key to Bob', file=sys.stderr)
+
+                        data = int(data.decode('utf-8'))
+                        PRIVATE_COMMOM_KEY = data ** PRIVATE_KEY % PUBLIC_ROOT
+
+                        print('PRIVATE COMMON KEY == {}'.format(PRIVATE_COMMOM_KEY))
+
+                        connection.sendall(bytes(public_key, 'UTF-8'))
                     else:
                         print('no more data from', client_address, file=sys.stderr)
                         break
+                #---------------------------------------
+
+                #Receving cripto data
+                while True:
+
+                    data = connection.recv(32)
                     
-            finally:
-                connection.close()
+                    if data:
+                        if(data.decode('utf-8') == "ACK"):
+                            break
+
+                        print('Received encrypted message: ' % data, file=sys.stderr)
+
+                        data = data.decode('utf-8')
+                        decrypted_message = xor_cypher(data, str(PRIVATE_COMMOM_KEY))
+                        
+                        print('Decrypted message: {}'.format(decrypted_message), file=sys.stderr)
+                    else:
+                        print('no more data from', client_address, file=sys.stderr)
+                        break
+                
+                
+    
+
+        
+    def xor_cypher(string, key):
+
+        def xor(bit1, bit2):
+            return (bit1 + bit2) - 2*(bit1*bit2)
+
+        if len(string) > len(key):
+            key += (len(string) - len(key)) * '@'
+        else:
+            string += ( len(key) - len(string)) * '@'
+        
+        out = bitarray()
+
+        bit_string = bitarray()
+        bit_key = bitarray()
+
+        bit_string.frombytes(string.encode('utf-8'))
+        bit_key.frombytes(key.encode('utf-8'))
+
+        for S_bit, K_bit in zip(bit_string, bit_key):
+            out.append(xor(S_bit, K_bit))
+
+        out = bitarray(out.tolist()).tobytes().decode('utf-8')
+
+        return out
+
+            
 
     return start
 
+
 a = server(SERVER_PORT)
 a()
+
+# chanel = a("decrypt","hate")
+# print('love xor hate = {}'.format(chanel))
+# print('{} xor  hate= {}'.format(chanel, a(chanel, 'hate')))
 
 
